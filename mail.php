21c0c527d8b16a2f952a09741b1c5593a08426fb
Quentin Goinaud <quentin.goinaud@gmail.com>
2016-03-10 18:17:24 +0100

    -

--- mail.php
+++ mail.php
@@ -13,15 +13,15 @@ $mail = new PHPMailer;
 $mail->SMTPDebug = 2;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->Host = $obj["smtp_server"];  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'quentin.goinaud@gmail.com';                 // SMTP username
-$mail->Password = '123';                           // SMTP password
+$mail->Username = $obj["from_mail"];                 // SMTP username
+$mail->Password = $obj["password"];                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+$mail->Port = $obj["port"];                                    // TCP port to connect to
 
-$mail->setFrom('quentin.goinaud@gmail.com', 'Armaldio');
-$mail->addAddress('armaldio@gmail.com', 'Joe User');     // Add a recipient
+$mail->setFrom($obj["from_mail"], $obj["name_from"]);
+$mail->addAddress($obj["recipient"], $obj["recipient"]);     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -31,13 +31,13 @@ $mail->addAddress('armaldio@gmail.com', 'Joe User');     // Add a recipient
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+$mail->Subject = $obj["subject"];
+$mail->Body    = $obj["body"];
+$mail->AltBody = $obj["body"];
 
 if (!$mail->send()) {
     echo 'Message could not be sent.';
     echo 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
-    echo 'Message has been sent';
+    echo 'OK';
 }
